9dae7b4dc1ccbc24e8ff27e90ea0c548c03f86f3
SerebryakovMA <serebryakov17m@mail.ru>
2021-06-23 21:19:24 +0300

    split-step method realisation with fft

--- pde.py
+++ pde.py
@@ -15,12 +15,17 @@ from matplotlib import colors
 from matplotlib.ticker import PercentFormatter
 
 def TDMAsolver(a, b, c, d):
-    ac, bc, cc, dc = map(np.array, (a, b, c, d))
+    #ac, bc, cc, dc = map(np.array, (a, b, c, d), dtype=np.cdouble)
+    ac = np.array(a, dtype = np.cdouble)
+    bc = np.array(b, dtype = np.cdouble)
+    cc = np.array(c, dtype = np.cdouble)
+    dc = np.array(d, dtype = np.cdouble)
+
     for i in range(1, len(d)): # assume a, b, c as diagonal elements, d as free terms column
         mc = ac[i-1]/bc[i-1]
         bc[i] -= mc*cc[i-1] 
         dc[i] -= mc*dc[i-1]
-        	    
+              
     xc = bc
     xc[-1] = dc[-1]/bc[-1]
 
@@ -39,8 +44,8 @@ num_x = 1000
 dx = 2*x_0/num_x
 num_t = 10
 u = np.array([math.exp(-10*np.power(x,2)/(num_x**2)) for x in range(-x_0*num_x, x_0*num_x, 1)])
-u_prev = np.array([-dt/(2*np.power(dx,2)) for x in range(-x_0*num_x, x_0*num_x - 1, 1)])
-u_i = np.array([1j + dt/(2*np.power(dx,2)) for x in range(-x_0*num_x, x_0*num_x, 1)])
+u_prev = np.array([dt/(2*np.power(dx,2))*1j for x in range(-x_0*num_x, x_0*num_x - 1, 1)])
+u_i = np.array([1 - dt/(np.power(dx,2))*1j for x in range(-x_0*num_x, x_0*num_x, 1)])
 u_next = u_prev
 
 u_list = u_i.reshape(1, u_i.shape[0])
@@ -48,14 +53,38 @@ u_list = u_i.reshape(1, u_i.shape[0])
 for t in range(1, num_t):
     sol1 = TDMAsolver(u_prev, u_i, u_next, u)
     #np.vectorize(sol1)
-    u = (sol2(sol1, dt)).real
+    u = (sol2(sol1, dt))
     u_list = np.vstack((u_list, u))
 
+u_list = np.real(u_list)
 print(u_list.shape)
-x = [dx*(-num_x + num) for num in range(num_x)]
+# print(u_list[0, 0])
+x = [dx*num for num in range(-x_0*num_x, x_0*num_x, 1)]
 t = np.array([dt*num for num in range(num_t)])
 x, t = np.meshgrid(x,t)
 print(x.shape)
 fig = plt.figure()
 ax = Axes3D(fig)
+
 ax.plot_surface(x, t, u_list)
+plt.savefig("pde_real.pdf")
+
+u = np.array([[math.exp(-10*np.power(x,2)/(num_x**2)) for x in range(-x_0*num_x, x_0*num_x, 1)]])
+def split_step(u, num_x, num_t, dt, dx, x_0):
+    k = np.array([dx*num for num in range(-x_0*num_x, x_0*num_x, 1)])
+    for t in range(num_t):
+        for i in range(num_x):
+            u[-1][i] *= cmath.exp(1j*np.abs(u[-1][i])*dt)
+
+        u[-1] = np.fft.fft(u[-1])
+
+        for i in range(num_x):
+            u[-1][i] *= cmath.exp(-1j*np.power(k[i],2)*dt)
+
+        u[-1] = np.fft.ifft(u[-1])
+    u = np.vstack((u, u[-1]))
+u = np.cdouble(u)
+split_step(u, num_x, num_t, dt, dx, x_0)
+ax.plot_surface(x, t, np.real(u))
+print(u.shape)
+plt.savefig("pde_fft.pdf")
